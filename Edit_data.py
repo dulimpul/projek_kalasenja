bd89eaef4531813c14563f51afa4dec6a0106779
dulimpul <duckg.diger46@gmail.com>
2021-10-08 23:45:47 +0800

    update

--- Edit_data.py
+++ Edit_data.py
@@ -84,10 +84,3 @@ def editUi():
     else:
         dataUiNew = float(input("Masukan data yang baru: "))
         data.Ui[indeksUi] = dataUiNew
-
-
-
-def kalimantan_love():
-    print('WELCOME KALIMANTAN')
-
-kalimantan_love()

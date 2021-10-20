f8aa0d3fc4914e8bd6135773a1c9b7b04232f917
dulimpul <duckg.diger46@gmail.com>
2021-10-20 23:11:13 +0800

    update

--- hitung_core/menu_hitung.py
+++ hitung_core/menu_hitung.py
@@ -1,6 +1,7 @@
 import menu
 import os
 import data
+import numpy as np
 
 def menuhitung():
     print("="*50, "MENU HITUNG", "="*50)
@@ -20,9 +21,15 @@ def menuhitung():
         SemuaTabel()
         print('\n')
         input('enter to countinue')
+        os.system('clear')
         menuhitung()
     elif keyhit == 2:
-        print("lorem ip sum")
+        os.system('clear')
+        A_B_C_nilia()
+        print('\n')
+        input('enter to countinue')
+        os.system('clear')
+        menuhitung()
     elif keyhit == 3:
         print("lorem ip sum")
     elif keyhit == 4:
@@ -38,23 +45,26 @@ def menuhitung():
         os.system("clear") # Linux
         menu.menu()
 
-
+# fungsi tahun
 def TmpilTabeldataTh():
-    tahunTB = len(data.tahun) 
-    return tahunTB
+    tahun_akhir = len(data.tahun) 
+    return tahun_akhir
   
-
+# fungsi yi
 def TmpilanTabelYi():
     YiTB = data.Yi.copy()
     hasilTByi = sum(YiTB)
     return hasilTByi
 
+# fungsi ui
 def TmpilanTbelUi():
     UiTB = data.Ui.copy()
     hasilTbui = sum(UiTB)
     return hasilTbui
 
+# fungsi ui pangkat 2
 def Uipangakat2(lihatHsil2='null'):
+    global hasil2
     UiTB2 = data.Ui.copy()
     my_new_list = []
     for i in UiTB2:
@@ -67,7 +77,9 @@ def Uipangakat2(lihatHsil2='null'):
     else:
         return my_new_list
 
+# fungsi Ui pangkat 4
 def Uipangakat4(lihatHsil='null'):
+    global hasil4
     UiTB4 = data.Ui.copy()
     my_new_list = []
     for u4 in UiTB4:
@@ -79,7 +91,37 @@ def Uipangakat4(lihatHsil='null'):
         return hasil4
     else:
         return my_new_list
+
+# fungsi menghitung ui dan yi 
+def menghitungYIdanUI():
+    global HasilAkhirUIYI
+    YIclone = data.Yi.copy()
+    Uiclone = data.Ui.copy()
+
+    a = np.array(YIclone)
+    b = np.array(Uiclone)
     
+    yi_kali_Ui = a * b
+    HasilAkhirUIYI = sum(yi_kali_Ui)
+    print('HASIL PERKALIAN YI DAN UI ADALAH =',yi_kali_Ui)
+    print('TOTAL PERKALIAN YI DAN UI ADALAH =', HasilAkhirUIYI)
+
+# fungsi menghitung yi dan ui pangakat 2
+def MenghitungYidanUiPangakat():
+    global HasilAkhirUIYI2
+    YIclone2 = data.Yi.copy()
+    YiKaliUi2 = Uipangakat2(lihatHsil2='null').copy()
+
+    a = np.array(YIclone2)
+    b = np.array(YiKaliUi2)
+    yi_kali_ui2 = a * b
+    HasilAkhirUIYI2 = sum(yi_kali_ui2)
+
+    print('HASIL  PERKALIAN YI DAN UI PANGKAT 2 ADALAH =',yi_kali_ui2)
+    print('TOTAL  PERKALIAN YI DAN UI PANGKAT 2 ADALAH =', HasilAkhirUIYI2)
+
+
+# fungsi menapilkana hasil jumlah tabel
 def SemuaTabel():
     #jumlah banyak tabel
     a = TmpilTabeldataTh()
@@ -131,34 +173,50 @@ def SemuaTabel():
     print('='*50)
     MenghitungYidanUiPangakat()
 
-def menghitungYIdanUI():
-    YIclone = data.Yi.copy()
-    Uiclone = data.Ui.copy()
+# =============================================new fungsi============================================================
 
-    yi_kali_Ui = []
 
-    for hitung in YIclone:
-        hitung2 = hitung
-    for hitung3 in Uiclone:
-        yi_kali_Ui.append(hitung2 * hitung3)
+def mencarinilai_B():
+    vaktorA = HasilAkhirUIYI / hasil2
+    return vaktorA
+def mencarinilai_C():
 
-    HasilAkhirUIYI = sum(yi_kali_Ui)
-    print('HASIL PERKALIAN YI DAN UI ADALAH =',yi_kali_Ui)
-    print('TOTAL PERKALIAN YI DAN UI ADALAH =', HasilAkhirUIYI)
+    # varibake satu 
+    tahun_y = TmpilTabeldataTh()
+    ui2_x = hasil2
+    yijumlah = TmpilanTabelYi()
 
-def MenghitungYidanUiPangakat():
-    YIclone2 = data.Yi.copy()
-    YiKaliUi2 = Uipangakat2(lihatHsil2='null').copy()
+    # varibek ke dua 
+    ui4_x = hasil4
+    yiUIpangkat4 = HasilAkhirUIYI2
 
-    yi_kali_ui2 = []
+#     solnya = 9x + 60y = 4.7
+#           60x + 708y =32.62
 
-    for HitungUI2 in YiKaliUi2:
-        UI2pangkat = HitungUI2
+    m_list = [[tahun_y,ui2_x], [ui2_x, ui4_x]]
     
-    for HitungUIYI2 in YIclone2:
-        yi_kali_ui2.append(HitungUIYI2 * UI2pangkat)
+    A = np.array(m_list)
+    B = np.array([yijumlah,yiUIpangkat4])
 
-    HasilAkhirUIYI2 = sum(yi_kali_ui2)
+    inv_A = np.linalg.inv(A)
+    C_hasil_akhir = inv_A.dot(B)
+    X = np.linalg.inv(A).dot(B)
+    return C_hasil_akhir[1]
 
-    print('HASIL  PERKALIAN YI DAN UI PANGKAT 2 ADALAH =',yi_kali_ui2)
-    print('TOTAL  PERKALIAN YI DAN UI PANGKAT 2 ADALAH =', HasilAkhirUIYI2)
+def mencari_nilai_A():
+    # varibel
+    tahun_A = TmpilTabeldataTh() # masih ada bug yakni di tahunya apabila hanya 1 data harus 2 data
+    ui2_A = hasil2
+    nilaiC_A = mencarinilai_C()
+    YIjumlah_A = TmpilanTabelYi()
+
+    # perhitungan
+    perkalina_Th_dan_nilaic = nilaiC_A * ui2_A
+    pengurangan_tahun = YIjumlah_A - perkalina_Th_dan_nilaic
+    NiliaAkhir_A = pengurangan_tahun / tahun_A
+    return NiliaAkhir_A
+
+def A_B_C_nilia(): 
+    print('A =', mencari_nilai_A())
+    print('B =', mencarinilai_B())
+    print('C =', mencarinilai_C())
